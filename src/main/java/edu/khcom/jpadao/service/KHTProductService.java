63dd7b60a7343e007759854d77fc3ef0f8375552
mochoping <hodolskm@gmail.com>
2025-01-21 17:51:45 +0900

    initial

---
+++ src/main/java/edu/khcom/jpadao/service/KHTProductService.java
@@ -0,0 +1,28 @@
+package edu.khcom.jpadao.service;
+
+
+import edu.khcom.jpadao.dao.KHTProduct;
+import edu.khcom.jpadao.repository.KHTProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KHTProductService {
+
+    @Autowired
+    private KHTProductRepository khtProductRepository;
+
+    public List<KHTProduct> findAll() {
+        return khtProductRepository.findAll();
+    }
+
+    public KHTProduct save(KHTProduct khtProduct) {
+        return khtProductRepository.save(khtProduct);
+    }
+
+    public KHTProduct findById(int id) {
+        return khtProductRepository.findById(id);
+    }
+}
